a26b7a698ec5b19bea79066f5c4f0b75552b4902
cldaly <clayton.daly91@gmail.com>
2020-02-05 10:24:07 -0500

    implement TodoService methods

--- src/main/java/com/cldaly/todos/repository/TodoRepository.java
+++ src/main/java/com/cldaly/todos/repository/TodoRepository.java
@@ -1,5 +1,7 @@
 package com.cldaly.todos.repository;
 
+import java.util.ArrayList;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import com.cldaly.todos.model.Todo;
 
 @Repository
 public interface TodoRepository extends MongoRepository<Todo,String> {
-
+	public ArrayList<Todo> findByDoneFalse();
+	public ArrayList<Todo> findByDoneTrue();
 }
--- src/main/java/com/cldaly/todos/service/TodoService.java
+++ src/main/java/com/cldaly/todos/service/TodoService.java
@@ -1,8 +1,12 @@
 package com.cldaly.todos.service;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.cldaly.todos.model.Todo;
 import com.cldaly.todos.repository.TodoRepository;
 
 @Service
@@ -10,4 +14,31 @@ public class TodoService {
 
 	@Autowired
 	private TodoRepository tr;
+	
+	public Todo saveTodo(Todo todo) {
+		todo.setDone(false);
+		return tr.save(todo);
+	}
+	
+	public Optional<Todo> findTodo(String id) {
+		return tr.findById(id);
+	}
+	
+	public ArrayList<Todo> findAllTodos(){
+		return tr.findByDoneFalse();
+	}
+	
+	public ArrayList<Todo> findAllCompleted(){
+		return tr.findByDoneTrue();
+	}
+	
+	public Todo completeTodo(String id) {
+		Todo t = tr.findById(id).get();
+		t.setDone(true);
+		return tr.save(t);
+	}
+	
+	public void deleteTodo(String id) {
+		tr.deleteById(id);
+	}
 }
